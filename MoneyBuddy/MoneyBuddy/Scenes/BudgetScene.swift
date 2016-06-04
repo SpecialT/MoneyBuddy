bc7d241d9589d2767d6fc6d08a2418318c0ba4f7
Samuel James Dominello <samueljamesdominello@Samuels-MacBook-Air.local>
2016-06-04 12:49:14 -0500

    commtt

--- MoneyBuddy/MoneyBuddy/Scenes/BudgetScene.swift
+++ MoneyBuddy/MoneyBuddy/Scenes/BudgetScene.swift
@@ -31,7 +31,7 @@ class BudgetScene: SuperScene {
         self.addChild(spriteNode)
         
         let sizeze = CGSize(width: 200, height: 200)
-        let button = ButtonNode(defaultImage: "wow", activeImage: "waddup", size: sizeze, buttonAction: introduceYourself)
+        let button = STButton(defaultImage: "wow", activeImage: "waddup", size: sizeze, buttonAction: introduceYourself)
         button.position.y = -150
         self.addChild(button)
     }
