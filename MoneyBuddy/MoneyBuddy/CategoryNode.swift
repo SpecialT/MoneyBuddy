1fb82554f0830b585d54896dada2e5203bf83dc8
Samuel James Dominello <samueljamesdominello@Samuels-MacBook-Air.local>
2016-06-05 23:31:19 -0500

    added initUI in CategoryNode

--- MoneyBuddy/MoneyBuddy/CategoryNode.swift
+++ MoneyBuddy/MoneyBuddy/CategoryNode.swift
@@ -8,9 +8,13 @@
 
 import SpriteKit
 
-class CategoryNode {
+class CategoryNode: SKSpriteNode {
     
-    private var name: String
+    override var name: String? {
+        didSet {
+        
+        }
+    }
     private var currentExpenses: CGFloat
     private var maxExpenses: CGFloat
     
@@ -23,9 +27,36 @@ class CategoryNode {
     }
     
     init(name: String, maxExpenses: CGFloat) {
-        self.name = name
         self.currentExpenses = 0
         self.maxExpenses = maxExpenses
+        super.init(texture: nil, color: SKColor.clearColor(), size: CGSizeZero)
+        
+        self.name = name
+        initUI()
+    }
+    
+    func initUI() {
+        
+        self.size = CGSizeMake(576, 150)
+        self.color = SKColor.greenColor()
+        
+        let nameLabel = SKLabelNode(fontNamed: "Kefa")
+        nameLabel.position = CGPointMake(-self.size.width / 2, 0)
+        nameLabel.zPosition = 1
+        nameLabel.text = name
+        nameLabel.fontSize = 60
+        nameLabel.fontColor = SKColor.blueColor()
+        nameLabel.horizontalAlignmentMode = .Left
+        nameLabel.verticalAlignmentMode = .Baseline
+        self.addChild(nameLabel)
+        
+        let addTransactionButton = STButton(defaultImage: "wow", activeImage: "waddup", size: CGSizeMake(120, 120), buttonAction: touchAddTransactionButton )
+        addTransactionButton.position = CGPointMake(self.size.width / 2 - 50, 0)
+        self.addChild(addTransactionButton)
+    }
+    
+    func touchAddTransactionButton() {
+    
     }
 }
 
--- MoneyBuddy/MoneyBuddy/Scenes/MenuScene.swift
+++ MoneyBuddy/MoneyBuddy/Scenes/MenuScene.swift
@@ -52,3 +52,4 @@ class MenuScene: SuperScene {
         
     }
 
+}
